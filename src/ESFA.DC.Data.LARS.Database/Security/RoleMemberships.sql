991f01d9e662e40332106f9f2edf230ee4095cd9
Peter Chamley <13586812+PJChamley@users.noreply.github.com>
2018-05-18 11:55:21 +0100

    DB : Add RW Account, FIX RO Account remove Write access.

--- src/ESFA.DC.Data.LARS.Database/Security/RoleMemberships.sql
+++ src/ESFA.DC.Data.LARS.Database/Security/RoleMemberships.sql
@@ -1,7 +1,10 @@
 ﻿
-ALTER ROLE [db_datawriter] ADD MEMBER [Lars_RO_User];
-
-
 GO
 ALTER ROLE [db_datareader] ADD MEMBER [Lars_RO_User];
+GO
+ALTER ROLE [db_datawriter] ADD MEMBER [Lars_RW_User];
+GO
+ALTER ROLE [db_datareader] ADD MEMBER [Lars_RW_User];
+GO
+
 
---
+++ src/ESFA.DC.Data.LARS.Database/Security/User_RW.sql
@@ -0,0 +1,7 @@
+﻿CREATE USER [Lars_RW_User]
+    WITH PASSWORD = N'$(LarsRWUserPwd)';
+GO
+
+GRANT CONNECT TO [Lars_RW_User]
+
+GO
