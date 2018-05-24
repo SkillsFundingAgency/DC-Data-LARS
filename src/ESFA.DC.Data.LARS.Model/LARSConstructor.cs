bd543cb8fe38a26429d2208f6761f53178cb5629
Mike Childs <mike.childs@education.gov.uk>
2018-05-24 10:20:03 +0100

    Added LARS Constructor Partial Class to allow the Connection String to be provided into the Context Constructor

---
+++ src/ESFA.DC.Data.LARS.Model/LARSConstructor.cs
@@ -0,0 +1,10 @@
+﻿namespace ESFA.DC.Data.LARS.Model
+{
+    public partial class LARS
+    {
+        public LARS(string connectionString)
+            : base(connectionString)
+        {
+        }
+    }
+}
