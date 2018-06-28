7c28db936c83ade9928d82eb7ae27bcac9123ea8
Peter Chamley <13586812+PJChamley@users.noreply.github.com>
2018-06-28 16:18:47 +0100

    SQL : Update Post Deployment Script.

--- src/ESFA.DC.Data.LARS.Database/zz.Script.PostDeployment.sql
+++ src/ESFA.DC.Data.LARS.Database/zz.Script.PostDeployment.sql
@@ -1,33 +1,43 @@
 ﻿/*
- Pre-Deployment Script Template							
+Post-Deployment Script Template							
 --------------------------------------------------------------------------------------
- This file contains SQL statements that will be executed before the build script.	
- Use SQLCMD syntax to include a file in the pre-deployment script.			
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
  Example:      :r .\myfile.sql								
- Use SQLCMD syntax to reference a variable in the pre-deployment script.		
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
  Example:      :setvar TableName MyTable							
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
 
-
+GO
 RAISERROR('		   Extended Property',10,1) WITH NOWAIT;
- 
+GO
+
 RAISERROR('		         %s - %s',10,1,'BuildNumber','$(BUILD_BUILDNUMBER)') WITH NOWAIT;
 IF NOT EXISTS (SELECT name, value FROM fn_listextendedproperty('BuildNumber', default, default, default, default, default, default))
 	EXEC sp_addextendedproperty @name = N'BuildNumber', @value = '$(BUILD_BUILDNUMBER)';  
 ELSE
 	EXEC sp_updateextendedproperty @name = N'BuildNumber', @value = '$(BUILD_BUILDNUMBER)';  
 	
+GO
 RAISERROR('		         %s - %s',10,1,'BuildBranch','$(BUILD_BRANCHNAME)') WITH NOWAIT;
 IF NOT EXISTS (SELECT name, value FROM fn_listextendedproperty('BuildBranch', default, default, default, default, default, default))
 	EXEC sp_addextendedproperty @name = N'BuildBranch', @value = '$(BUILD_BRANCHNAME)';  
 ELSE
 	EXEC sp_updateextendedproperty @name = N'BuildBranch', @value = '$(BUILD_BRANCHNAME)';  
 
-RAISERROR('		         %s - %s',10,1,'DeploymentDatetime','$(DEPLOYMENT_DATETIME)') WITH NOWAIT;
+GO
+DECLARE @DeploymentTime VARCHAR(35) = CONVERT(VARCHAR(35),GETUTCDATE(),113);
+RAISERROR('		         %s - %s',10,1,'DeploymentDatetime',@DeploymentTime) WITH NOWAIT;
 IF NOT EXISTS (SELECT name, value FROM fn_listextendedproperty('DeploymentDatetime', default, default, default, default, default, default))
-	EXEC sp_addextendedproperty @name = N'DeploymentDatetime', @value = '$(DEPLOYMENT_DATETIME)';  
+	EXEC sp_addextendedproperty @name = N'DeploymentDatetime', @value = @DeploymentTime;  
 ELSE
-	EXEC sp_updateextendedproperty @name = N'DeploymentDatetime', @value = '$(DEPLOYMENT_DATETIME)';  
+	EXEC sp_updateextendedproperty @name = N'DeploymentDatetime', @value = @DeploymentTime;  
 
+
+GO
+PRINT N'Update complete.';
+
+
+GO
